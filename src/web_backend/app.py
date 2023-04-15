20a6544d9198f78c62f1273a41dccf2f5ee8f381
Michael Abelar <abelar@michaels-mbp.myfiosgateway.com>
2023-04-14 22:53:41 -0400

    Create basic flow that on submission redirects to job page

--- src/web_backend/app.py
+++ src/web_backend/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+import random
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
+    if request.method == 'POST':
+        # todo: hook this up to a backend but for now we do a temporary redirect
+        job_id = random.randint(0, 1000000)
+        return redirect(url_for('job', job_id=job_id))
     return render_template('index.html')
+
+@app.route('/job/<job_id>')
+def job(job_id):
+    return render_template('job.html', job_id=job_id)
 
 if __name__ == '__main__':
     app.run()
