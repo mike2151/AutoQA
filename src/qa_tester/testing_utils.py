0c4afb8c2771d75fd79bb7c2d3864129623e5302
Michael Abelar <abelar@michaels-mbp.myfiosgateway.com>
2023-04-15 00:20:33 -0400

    Integrate OpenAI API

--- src/qa_tester/testing_utils.py
+++ src/qa_tester/testing_utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import openai
 
 # TODO: Hack until I figure out how the fuck Python imports work 
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -10,3 +11,11 @@ from web_backend.db.jobs_util import JobStatus, update_job_status
 
 def process_request(job_id, url, instructions):
     update_job_status(job_id, JobStatus.PLANNING_QA)
+    # TODO: secure this
+    openai.organization = os.environ.get("OPENAI_ORG")
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "assistant", "content": "Can you say the word hello?"}]
+    )
+    llm_response = chat.choices[0].message
+    print(llm_response)
